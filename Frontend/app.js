36dfa6748de9323b86da9d2320d1b29f10e7a8cc
JeevaganMohankumar <jeevaganmohankumar2@gmail.com>
2026-06-03 13:16:54 +0530

    Create app.js

---
+++ Frontend/app.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route }
+from "react-router-dom";
+
+import Products from "./Products";
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Products />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
